e2e7082bda553e17952ac0bd19cd5209720ca889
zero323 <matthew.szymkiewicz@gmail.com>
2015-10-29 21:21:13 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from distutils.core import setup
 setup(
     name='tryingsnake',
     version='0.1.0',
-    packages=[''],
+    packages=['tryingsnake'],
     url='https://github.com/zero323/tryingsnake',
     license='MIT',
     author='Maciej Szymkiewicz',
-    author_email='',
-    description=''
+    author_email='matthew.szymkiewicz@gmail.com',
+    description='A simple, Scala inspired, Try implementation.',
+    package_data={'tryingsnake': ['test/*.py']},
+    zip_safe=False
 )
