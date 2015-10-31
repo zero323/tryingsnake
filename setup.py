abcee6f891bac33e4fc360a123756ed37a406180
zero323 <matthew.szymkiewicz@gmail.com>
2015-10-31 18:32:20 +0100

    Change version to 0.2.1

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ from distutils.core import setup
 
 setup(
     name='tryingsnake',
-    version='0.2.0',
+    version='0.2.1',
     packages=['tryingsnake'],
     url='https://github.com/zero323/tryingsnake',
     license='MIT',
     author='Maciej Szymkiewicz',
     author_email='matthew.szymkiewicz@gmail.com',
-    description='A simple, Scala inspired, Try implementation.',
+    description='Exception handling, the functional way.',
     package_data={'tryingsnake': ['test/*.py']},
+    long_description=(open('README.rst').read() if exists('README.rst')
+                        else ''),
+    classifiers=['Development Status :: 3 - Alpha'],
     zip_safe=False
 )
