bb215c3a4d4f332cd4348ef165c46379dd92bb40
zero323 <mszymkiewicz@gmail.com>
2019-12-27 00:12:58 +0100

    Bump version to 0.4.0.dev0

--- tryingsnake/__init__.py
+++ tryingsnake/__init__.py
@@ -4,7 +4,7 @@ from typing import Any, Callable, Generic, Iterable, Tuple, Type, TypeVar
 T = TypeVar("T")
 U = TypeVar("U")
 
-__version__ = '0.3.1'
+__version__ = '0.4.0.dev0'
 
 
 class Try_(Generic[T]):
