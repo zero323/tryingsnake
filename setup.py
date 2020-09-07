f5d357f8eeb1e7631ee86db596b09379706350a2
zero323 <mszymkiewicz@gmail.com>
2020-09-07 22:25:35 +0200

    Include stub files in the distribution

--- setup.py
+++ setup.py
@@ -13,7 +13,10 @@ setup(
     author="Maciej Szymkiewicz",
     author_email="matthew.szymkiewicz@gmail.com",
     description="Exception handling, the functional way.",
-    package_data={"tryingsnake": ["test/*.py"]},
+    package_data={
+        "": ["*.pyi", "py.typed"],
+        "tryingsnake": ["test/*.py"]
+    },
     long_description=(open("README.md").read() if exists("README.md") else ""),
     long_description_content_type="text/markdown",
     classifiers=[
